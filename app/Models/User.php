4ed2f9d9e994d34b9199f6be2ac8dd2e6b89b5e3
firdhan29 <firdhanv@gmail.com>
2026-05-17 17:33:03 +0700

    fix: add email_verified_at to fillable in User model to fix MassAssignmentException in admin panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'summary', 'phone_wa', 'linkedin_url', 'github_url', 'location'])]
+#[Fillable(['name', 'email', 'password', 'summary', 'phone_wa', 'linkedin_url', 'github_url', 'location', 'email_verified_at'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
